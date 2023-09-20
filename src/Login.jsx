a50f4d1cd639b86476f39e7305797fec70974688
Aritra Dey <81703791+aritradey-CS@users.noreply.github.com>
2023-09-20 12:06:16 +0530

    First commit

---
+++ src/Login.jsx
@@ -0,0 +1,13 @@
+// src/components/Login.js
+import React from 'react';
+
+const Login = () => {
+  return (
+    <div className="section">
+      <h2>Login</h2>
+      {/* Add your login form here */}
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/Signup.jsx
@@ -0,0 +1,13 @@
+// src/components/Signup.js
+import React from 'react';
+
+const Signup = () => {
+  return (
+    <div className="section">
+      <h2>Create New Account</h2>
+      {/* Add your signup form here */}
+    </div>
+  );
+};
+
+export default Signup;
